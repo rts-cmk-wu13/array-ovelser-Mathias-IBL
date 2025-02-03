6b0045567d3a596c1804f9f29f1c41e8d8b6f54e
Mathias Larsen <mathiaslarsen1122@gmail.com>
2025-02-03 12:09:56 +0100

    Opgave 1 løst

--- arrays.js
+++ arrays.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
-
+    let newNumbers = [21, 26, 32, 45, 48, 55]
     // --- Løs opgaverne herunder --- 
+
+    // Opgave1
+
+    console.log("Opgave 1", newNumbers);
+    
 
 }) // ends DOMContentLoaded
